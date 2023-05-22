faa63c65f7140ec68d9db28b7d1e995a37fed2de
Michael Tims <michaeltims81@gmail.com>
2023-05-22 14:42:52 -0400

    chore: update to Angular 15 and SKY UX 8 (#51)
    * Updates for SKYUX 8
    
    * Revert skyux ci
    
    * Update skyux-ci.yml
    
    * Node 18.5
    
    ---------
    
    Co-authored-by: BLACKBAUD\Michael.Tims <Michael.Tims@blackbaud.com>

--- projects/addin-client/src/test.ts
+++ projects/addin-client/src/test.ts
@@ -8,26 +8,9 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-// Fix for crossvent "global is not defined" error. The crossvent library
-// is used by Dragula, which in turn is used by multiple SKY UX components.
-// See: https://github.com/bevacqua/dragula/issues/602
-(window as any).global = window;
-
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
   { teardown: { destroyAfterEach: true }},
 );
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
